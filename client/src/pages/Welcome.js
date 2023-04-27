4aea8dc79e420545af0ea5fd12c278d124a85941
Chris Hong <hongchris97@gmail.com>
2023-04-27 00:41:06 -0700

    Added welcome page

---
+++ client/src/pages/Welcome.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Welcome = () => {
+    return (
+        <>
+            <h1>Welcome to your Personal Finance App that will help you reach your $$ goals.</h1>
+            <button>Click Here to Begin</button>
+        </>
+    )
+}
