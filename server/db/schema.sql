4d87c0dea29f6a4c708cd8c917236ef9a784058f
Chris Hong <hongchris97@gmail.com>
2023-05-17 02:29:45 -0700

    Adding server

---
+++ server/db/schema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS finance_db;
+
+CREATE DATABASE finance_db;
+
+USE finance_db;
--- server/server.js
+++ server/server.js
@@ -1,2 +1,31 @@
 const express = require('express')
 const path = require('path')
+const session = require('express-session')
+
+const sequelize = require('./config/connection')
+const SequelizeStore = require('connect-session-sequelize')
+
+const app = express()
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: process.env.SESSION_SECRET || "super secret",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
+
+app.use(session(sess))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', routes)
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`))
+})
