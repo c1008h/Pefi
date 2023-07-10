a8101eb3ace382ed564fdf7edce31fb0c33fe5e7
Chris Hong <hongchris97@gmail.com>
2023-07-09 23:44:39 -0700

    Added monthly statement for income

--- client/src/components/IncomeBtn.js
+++ client/src/components/IncomeBtn.js
@@ -47,6 +47,7 @@ export const IncomeBtn = (showIncomeForm) => {
                   <option value="daily">Daily</option>
                   <option value="weekly">Weekly</option>
                   <option value="biweekly">Biweekly</option>
+                  <option value="monthly">Monthly</option>
                 </select>
               </label>
               <label>
