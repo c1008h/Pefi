5f1234c348321a41da93ba781d525b69247ece38
Chris Hong <hongchris97@gmail.com>
2023-07-21 04:15:43 -0700

    user info on profile page

--- client/src/components/Profile/ProfileForm.jsx
+++ client/src/components/Profile/ProfileForm.jsx
@@ -1,35 +1,68 @@
 import { Container, Card, Form, Button } from 'react-bootstrap'
-// import PropTypes from 'prop-types'; // Import PropTypes
+import PropTypes from 'prop-types'; // Import PropTypes
+import { useState } from 'react';
 
-export default function ProfileForm() {
-  return (
-    <Container>
-      <h2>Profile</h2>
-      <Card>
-        <Form>
-            <Form.Group>
-                <Form.Label>First Name:</Form.Label>
-                <Form.Control></Form.Control>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Last Name:</Form.Label>
-                <Form.Control></Form.Control>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Email:</Form.Label>
-                <Form.Control></Form.Control>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Password:</Form.Label>
-                <Form.Control></Form.Control>
-            </Form.Group>
-            <Button tyoe='submit'>Save</Button>
-        </Form>
-      </Card>
-    </Container>
-  )
+export default function ProfileForm({userData}) {
+    const [firstName, setFirstName] = useState(userData.firstName || '')
+    const [lastName, setLastName] = useState(userData.lastName || '')
+    const [email, setEmail] = useState(userData.email || '')
+    const [password, setPassword] = useState()
+
+    console.log(userData.email)
+    console.log(email)
+    function saveFirst(e) {
+        setFirstName(e.target.value)
+    }
+    function saveLast(e) {
+        setLastName(e.target.value)
+    }
+    function saveEmail(e) {
+        setEmail(e.target.value)
+    }
+    function savePassword(e){
+        setPassword(e.target.value)
+    }
+
+    return (
+        <Container>
+        <h2>Profile</h2>
+        <Card>
+            <Form>
+                <Form.Group>
+                    <Form.Label>First Name:</Form.Label>
+                    <Form.Control
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Last Name:</Form.Label>
+                    <Form.Control             
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Email:</Form.Label>
+                    <Form.Control 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control></Form.Control>
+                </Form.Group>
+                <Button tyoe='submit'>Save</Button>
+            </Form>
+        </Card>
+        </Container>
+    )
 }
 
-// ProfileForm.PropTypes = {
-//     userData: PropTypes.array.isRequired
-// }
+ProfileForm.propTypes = {
+    userData: PropTypes.object.isRequired
+}
