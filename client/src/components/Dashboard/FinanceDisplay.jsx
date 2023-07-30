6b954290c51a883941369a2cf3ce5e40a5e620c4
Chris Hong <hongchris97@gmail.com>
2023-07-30 16:56:19 -0700

    displaying networth

--- client/src/components/Dashboard/FinanceDisplay.jsx
+++ client/src/components/Dashboard/FinanceDisplay.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types'; // Import PropTypes
 
-export default function FinanceDisplay() {
+export default function FinanceDisplay({userData}) {
+  const [networth, setNetworth] = useState()
+  const [monthlyIncome, setMonthlyIncome] = useState()
+  const [monthlyExpense, setMonthlyExpense] = useState()
+
+  useEffect(() => {
+    let digital = parseFloat(userData.financeGroup[0].digital)
+    let cash = parseFloat(userData.financeGroup[0].cash)
+    let saved = parseFloat(userData.financeGroup[0].saved)
+    let invested = parseFloat(userData.financeGroup[0].invested)
+
+    setNetworth((digital + cash + saved + invested).toLocaleString())
+  }, [userData])
+
+
+
+  console.log(userData.financeGroup[0])
+
   return (
     <div>
-        <h5>Net Worth:</h5>
+        <h5>Net Worth: {networth}</h5>
         <h5>Monthly Income:</h5>
         <h5>Monthly Expense:</h5>
 
     </div>
   )
+}
+
+FinanceDisplay.propTypes = {
+  userData: PropTypes.object.isRequired
 }
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ExpenseBtn } from '../components/Dashboard/ExpenseBtn.jsx'
 import { IncomeBtn } from '../components/Dashboard/IncomeBtn.jsx';
 import { CalendarEl } from '../components/Dashboard/Calendar.jsx'
 import FinanceDisplay from '../components/Dashboard/FinanceDisplay.jsx'
+import { useMutation, useQuery } from '@apollo/client';
+import { QUERY_ME } from '../utils/queries'
+import { authService } from '../utils/auth';
 
 export const Dashboard = () => {
+    const [userData, setUserData] = useState({})
     const [showForm, setShowForm] = useState(false);
     const [showExpenseForm, setShowExpenseForm] = useState(false);
     const [showIncomeForm, setShowIncomeForm] = useState(false);
-  
+    const [loading, setLoading] = useState(true)
+    const { data } = useQuery(QUERY_ME)
+
+    useEffect(() => {
+        if (data) {
+          setUserData(data.me)
+          setLoading(false)
+        }
+    }, [data])
+
+    // console.log(userData)
+
     const openExpenseForm = () => {
       setShowExpenseForm(true);
       setShowIncomeForm(false);
@@ -24,13 +39,24 @@ export const Dashboard = () => {
       setShowForm(false);
     };
 
+    const token = authService.loggedIn() ? authService.getToken() : null;
+    if(!token) {
+        return <h2>Please login first</h2>
+    }
+    if(loading) {
+        return <h2>LOADING...</h2>
+    }
     return (
         <div className='container' style={{flexDirection:'row'}}>
             <h2>Dashboard</h2>
             <div >
                 <div style={{display:'flex'}}>
-                    <CalendarEl />
-                    <FinanceDisplay />
+                    <CalendarEl 
+                        userData={userData}
+                    />
+                    <FinanceDisplay 
+                        userData={userData}
+                    />
                 </div>
                 <button
                     onClick={openExpenseForm}
@@ -47,8 +73,13 @@ export const Dashboard = () => {
             </div>
 
             <div style={{width:'60%'}}>
-                {showExpenseForm && <ExpenseBtn />}            
-                {showIncomeForm && <IncomeBtn />}
+                {showExpenseForm && 
+                <ExpenseBtn 
+                />}            
+                {showIncomeForm && 
+                <IncomeBtn 
+
+                />}
             </div>     
                       
         </div>
