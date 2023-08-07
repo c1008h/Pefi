a1b162ea8d168923fbeb4fe2f76a1bdaf2381345
Chris Hong <hongchris97@gmail.com>
2023-08-06 17:03:25 -0700

    updating styling for welcome page

--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom'
+import AuthService from '../utils/auth'
 
 export const Welcome = () => {
     return (
-        <>
+        <div style={{margin:'5px', padding: '5px'}}>
             <h1>Welcome to your Personal Finance App that will help you reach your $$ goals.</h1>
             
             <Link to='/signup'>
                 <button>Click Here to Begin</button>
             </Link>
-        </>
+        </div>
     )
 }
