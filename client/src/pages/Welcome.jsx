9d37dca4119d3a0c97e9dc6db535fb2f1845928f
Chris Hong <hongchris97@gmail.com>
2023-08-08 00:30:46 -0700

    display dashboard instead of welcome if user is logged in

--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -1,13 +1,21 @@
 import { Link } from 'react-router-dom'
+import  {authService } from '../utils/auth'
+import { Dashboard } from './Dashboard';
 
 export const Welcome = () => {
     return (
         <div style={{margin:'5px', padding: '5px'}}>
-            <h1>Welcome to your Personal Finance App that will help you reach your $$ goals.</h1>
-            
-            <Link to='/signup'>
-                <button>Click Here to Begin</button>
-            </Link>
+            {authService.loggedIn() ? (
+                <Dashboard/>
+                ) : (
+                <div>
+                    <h1>Welcome to your Personal Finance App that will help you reach your $$ goals.</h1>
+                
+                    <Link to='/signup'>
+                        <button>Click Here to Begin</button>
+                    </Link>
+                </div>
+            )}
         </div>
     )
 }
