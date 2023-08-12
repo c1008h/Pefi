35048d5664c78d6f03af6aebd4ebe4750da4d87e
Chris Hong <hongchris97@gmail.com>
2023-08-12 15:53:41 -0700

    moved packages to client

--- client/src/components/Dashboard/Graphs/Monthly.jsx
+++ client/src/components/Dashboard/Graphs/Monthly.jsx
@@ -3,7 +3,12 @@ import { Bar } from "react-chartjs-2";
 export default function Monthly() {
   return (
     <div>
-      
+        <h1>Monthly bar</h1>
+      {/* <Bar 
+        // data={{
+        //     labels: ['']
+        // }}
+      /> */}
     </div>
   )
 }
