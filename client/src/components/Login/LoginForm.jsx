4cf770b382d910096cc6f622dd1a924826332a8d
Chris Hong <hongchris97@gmail.com>
2023-08-15 01:15:27 -0700

    styling for signup page

--- client/src/components/Login/LoginForm.jsx
+++ client/src/components/Login/LoginForm.jsx
@@ -1,59 +1,67 @@
-import { Form, Button, Card } from 'react-bootstrap'
-import {Link} from 'react-router-dom'
+import { Form, Button, Container, Row, Col, Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'; // Import PropTypes
 
 export default function LoginForm({ handleFormSubmit, handleChange, formState }) {
-  return (
+    return (
+      <Container fluid='true'>
+        <Row>
+          <Col>
+          </Col>
 
-    <Form onSubmit={handleFormSubmit} style={{padding:'15%', justifyContent:'center'}}>
-        <h1>Login</h1>
 
-        <Form.Group>
-          <Form.Label>Email</Form.Label>
-          <Form.Control 
-            type="email" 
-            placeholder="Enter email" 
-            name='email'
-            value={formState.email} 
-            onChange={handleChange}
-            required
-            isInvalid={!formState.email}
-          />
-          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+          <Col>
+            <Form onSubmit={handleFormSubmit} style={{padding:'15%', justifyContent:'center'}}>
+              <h1>Login</h1>
+                <Form.Group>
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control 
+                    type="email" 
+                    placeholder="Enter email" 
+                    name='email'
+                    value={formState.email} 
+                    onChange={handleChange}
+                    required
+                    isInvalid={!formState.email}
+                  />
+                  <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
 
-        </Form.Group>
+                </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control 
-            type="password" 
-            placeholder="Password" 
-            value={formState.password}
-            name='password' 
-            onChange={handleChange}
-            required
-            isInvalid={!formState.password} 
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control 
+                    type="password" 
+                    placeholder="Password" 
+                    value={formState.password}
+                    name='password' 
+                    onChange={handleChange}
+                    required
+                    isInvalid={!formState.password} 
 
-          />
-        </Form.Group>
+                  />
+                </Form.Group>
 
-        <Form.Text className="text-muted">
-            We&apos;ll never share your email with anyone else.
-        </Form.Text>
-        <div>
-          <Button 
-            variant="success" 
-            type="submit" 
-            disabled={!(formState.email && formState.password)}
-            >
-          Submit
-          </Button>
-          <Link>Login</Link>
-        </div>
+                <Form.Text className="text-muted">
+                    We&apos;ll never share your email with anyone else.
+                </Form.Text>
+                <div>
+                  <Button 
+                    variant="success" 
+                    type="submit" 
+                    disabled={!(formState.email && formState.password)}
+                    >
+                  Submit
+                  </Button>
+                  <Link>Login</Link>
+                </div>
+              </Form>
+          </Col>
+        </Row>
+      
+      </Container>
 
-      </Form>
-
-  )
+    )
 }
 
 
--- client/src/components/Signup/SignupForm.jsx
+++ client/src/components/Signup/SignupForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom'
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Container, Row, Col, Card } from 'react-bootstrap';
 import PropTypes from 'prop-types'; // Import PropTypes
 
 export const SignupForm = ({ onSubmit }) => {
@@ -21,58 +21,72 @@ export const SignupForm = ({ onSubmit }) => {
         setConfirmPassword('')
       }
     };
+    return (
+      <Container fluid='true' >
+        <Row>
+          <Col style={{padding:'5%'}}>
+            <h1>Create your login</h1>
+            <h5>We&apos;ll need your email address, and a unique password. You&apos;ll use this to login to access Personal Finance next time.</h5>
+          </Col>
+          <Col>
+            <Card>
+              <Form onSubmit={handleSubmit} style={{padding:'15%', justifyContent:'center'}}>
+                <h2 style={{textAlign:'center'}}>Signup</h2>
+                <Form.Group>
+                  <Form.Label htmlFor='email'>Email</Form.Label>
+                  <Form.Control
+                    type='email'
+                    placeholder='Your email address'
+                    name='email'
+                    onChange={e => setEmail(e.target.value)}
+                    value={email}
+                    required
+                  />
+                  <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+                </Form.Group>
+          
+                <Form.Group>
+                  <Form.Label htmlFor='password'>Password</Form.Label>
+                  <Form.Control
+                    type='password'
+                    placeholder='Your password'
+                    name='password'
+                    onChange={e => setPassword(e.target.value)}
+                    value={password}
+                    required
+                  />
+                  <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label htmlFor='password2'>Confirm Password</Form.Label>
+                  <Form.Control
+                    type='password'
+                    placeholder='Confirm password'
+                    name='confirmPassword'
+                    onChange={e => setConfirmPassword(e.target.value)}
+                    value={confirmPassword}
+                    required
+                  />
+                  <Form.Control.Feedback type='invalid'>Confirm your password!</Form.Control.Feedback>
+                </Form.Group>
+                <Container>
 
-    return(
-        <Form onSubmit={handleSubmit} style={{padding:'15%', justifyContent:'center'}}>
-        <h2>Signup</h2>
-        <Form.Group>
-          <Form.Label htmlFor='email'>Email</Form.Label>
-          <Form.Control
-            type='email'
-            placeholder='Your email address'
-            name='email'
-            onChange={e => setEmail(e.target.value)}
-            value={email}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-        </Form.Group>
-  
-        <Form.Group>
-          <Form.Label htmlFor='password'>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Your password'
-            name='password'
-            onChange={e => setPassword(e.target.value)}
-            value={password}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group>
-          <Form.Label htmlFor='password2'>Confirm Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Confirm password'
-            name='confirmPassword'
-            onChange={e => setConfirmPassword(e.target.value)}
-            value={confirmPassword}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Confirm your password!</Form.Control.Feedback>
-        </Form.Group>
-        <div>
-            <Button style={{margin:'5%', justifyContent:'center', textAlign:'center'}}
-                disabled={!(email && password && confirmPassword)}
-                type='submit'
-                variant='success'>
-                Submit
-            </Button>
-            <Link to='/login'>Login</Link>
-        </div>
-
-      </Form>
+                    <p style={{font:'black'}}>Already started?</p>
+                    <Link to='/login'>Login to complete your application</Link>
+ 
+                    <p style={{font:'black'}}>By continuing, you agree to the USER ACCOUNT AGREEMENT and PRIVACY POLICY</p>
+                    <Button style={{margin:'5%', justifyContent:'center', textAlign:'center'}}
+                      disabled={!(email && password && confirmPassword)}
+                      type='submit'
+                      variant='success'>
+                      Submit
+                  </Button>
+                </Container>
+              </Form>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     )
 }
 
