0dcd80d01bf23fd53d232b982c8b8ae80dd720e7
Chris Hong <hongchris97@gmail.com>
2023-08-25 01:38:29 -0700

    checkPassword mutation can verify if user exist and if password is correct

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -116,7 +116,7 @@ const typeDefs = `
         addUser(email: String!, password: String!): Auth
         updateUser(email: String, firstName: String!, lastName: String!, location: String, gender: String, incomeLevel: String, birthday: String): User
         deleteUser(user_id: ID!, email: String!, reason: String!): User
-        checkPassword(password: String!): Auth
+        checkPassword(_id: ID!, password: String!): Auth
 
 
         createGoals(input: GoalInput!): User
