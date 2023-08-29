7ab60ea141cd2dae0655d2d562c119c42511a02e
Chris Hong <hongchris97@gmail.com>
2023-08-29 02:20:00 -0700

    progress bars renders percentage of how far the user is to achieving their goal

--- client/src/pages/Goals.jsx
+++ client/src/pages/Goals.jsx
@@ -8,6 +8,7 @@ import GoalCarousel from '../components/Goals/GoalCarousel'
 import { AllDisplay, CurrentDisplay, FutureDisplay, PreviousDisplay } from '../components/Goals/Displays'
 import { authService } from '../utils/auth'
 import { Container, Row, Col, Tabs, Tab } from 'react-bootstrap'
+import '../style/goals.css'
 
 export default function Goals() {
   const [userData, setUserData] = useState()
