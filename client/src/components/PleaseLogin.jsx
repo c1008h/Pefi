6f92b9fc964e5e2b881dc545e8de907ef8e6319c
Chris Hong <hongchris97@gmail.com>
2023-10-06 17:55:16 -0700

    styling for pleaselogin page

--- client/src/components/PleaseLogin.jsx
+++ client/src/components/PleaseLogin.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 
 export default function PleaseLogin() {
+
+
   return (
-    <div>
+    <div style={{height:'900px', background:'white', textAlign:'center', paddingTop:'5%'}}>
       <h2>Please login first!</h2>
+      <Link to='/login'>Click here to login</Link>
     </div>
   )
 }
