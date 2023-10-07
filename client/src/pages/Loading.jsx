bf9c3f26542b56b5c2b5d59caa76fcbbc95bd065
Chris Hong <hongchris97@gmail.com>
2023-10-07 02:11:05 -0700

    styling for loading

--- client/src/pages/Loading.jsx
+++ client/src/pages/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Loading() {
   return (
-    <div style={{backgroundColor:'white'}}>
+    <div style={{height:'900px', backgroundColor:'white', paddingTop:'5%'}}>
       <h2>LOADING...</h2>
     </div>
   )
